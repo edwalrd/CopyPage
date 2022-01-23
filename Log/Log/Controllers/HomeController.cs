f49ab888516a4c4d49b394e664220947b2d1b106
Edwal Tejada <edwallg@gmail.com>
2022-01-23 01:33:53 -0400

    Log

---
+++ Log/Log/Controllers/HomeController.cs
@@ -0,0 +1,41 @@
+﻿using Log.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Log.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            _logger.LogInformation(
+                $"{nameof(HomeController)}: {DateTime.Now.ToString("dd/MM/yyyy HH:mm:ss")} \n \n " +
+                $" Se ha iniciado esta Vista. \n \n");
+
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
